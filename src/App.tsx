ead36be995d81395f2a91533a32718a1582b06e4
Vinicyos Ferreira <vinifr987@gmail.com>
2025-09-10 21:20:31 -0300

    feat: add AboutMe component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Header from './components/app/Header';
 import Hero from './components/app/Hero';
+import AboutMe from './components/app/AboutMe';
 
 function App() {
   return (
     <div>
       <Header />
       <Hero />
+      <AboutMe/>
     </div>
   );
 }
---
+++ src/components/app/AboutMe.tsx
@@ -0,0 +1,25 @@
+const AboutMe = () => {
+  return (
+    <div className="flex flex-col items-center justify-center mt-20">
+      <div>
+        <h2 className="font-bold text-primary text-2xl">Sobre</h2>
+      </div>
+      <div className="w-5/6 space-y-5 text-sm">
+        <p className="mt-2">
+          Olá me chamo Vinicyos Ferreira, sou apaixonado por tecnologia
+          e inovação. Me identifiquei pela TI aos 17 anos, quando comecei a
+          pesquisar no Youtube sobre a área. Experiência de 1 ano com projetos em React,Javascript e Typescript, 
+         , com estudos contínuos em Node.js e SQL para atuar com aplicações fullstack.
+        </p>
+        <p>
+          Sempre buscando aperfeiçoar para aplicar métodos, técnicas e padrões
+          alinhados com as melhores práticas do mercado. Meu objetivo é
+          desenvolver projetos robustos e escaláveis no âmbito tecnológico,
+          visando entregar resultados que agreguem valor às pessoas.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;

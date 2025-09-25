4e550537e230ee5aea52d195173ea960bf0c98b9
Vinicyos Ferreira <vinifr987@gmail.com>
2025-09-25 14:14:49 -0300

    feat: adding new projects to the component

--- src/components/app/Projects.tsx
+++ src/components/app/Projects.tsx
@@ -4,6 +4,7 @@ import {
   CardHeader,
   CardTitle,
   CardContent,
+  CardFooter
 } from '@/components/ui/card';
 
 import { FaGithub } from 'react-icons/fa';
@@ -17,56 +18,59 @@ const Projects = () => {
       <h2 className="text-primary text-2xl font-bold text-center my-5">
         Projetos
       </h2>
-      {PROJECTS.map((project) => (
-        <Card
-          className="flex flex-col w-[95%] mx-auto relative"
-          key={project.id}
-        >
-          <CardHeader>
-            <CardTitle className="text-center font-semibold text-lg">
-              {project.name}
-            </CardTitle>
-            <CardDescription>{project.description}</CardDescription>
-          </CardHeader>
-          <CardContent>
-            <div className="relative group">
-              <img
-                src={project.imgURL}
-                alt="Capa da imagem do projeto News Finder"
-              />
-              <div
-                className="absolute inset-0 opacity-0 
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+        {PROJECTS.map((project) => (
+          <Card
+            className="flex flex-col w-[95%] mx-auto relative"
+            key={project.id}
+          >
+            <CardHeader>
+              <CardTitle className="text-center font-semibold text-lg">
+                {project.name}
+              </CardTitle>
+              <CardDescription>{project.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <div className="relative group">
+                <img
+                  src={project.imgURL}
+                  alt="Capa da imagem do projeto News Finder"
+                />
+                <div
+                  className="absolute inset-0 opacity-0 
                   group-hover:opacity-100 transition-opacity ease-in-out duration-500 
                   delay-75 backdrop-blur flex flex-col items-center justify-center gap-3"
-              >
-                <div
-                  className="bg-accent cursor-pointer p-2 w-45 rounded-lg"
                 >
-                  <a
-                    href={project.repositorieURL}
-                    className="text-primary font-semibold flex justify-center items-center gap-2"
-                    target="_blank"
-                  >
-                    <span>Ver repositório</span>
-                    <FaGithub size={35} />
-                  </a>
-                </div>
+                  <div className="bg-accent cursor-pointer p-2 w-45 rounded-lg">
+                    <a
+                      className="text-primary font-semibold flex justify-center items-center gap-2"
+                      target="_blank"
+                    >
+                      <FaGithub size={30} className="text-white" />
+                      <span>Ver repositório</span>
+                    </a>
+                  </div>
 
-                <div className="bg-accent cursor-pointer w-45 p-2 rounded-lg">
-                  <a
-                    href={project.demoURL}
-                    className="text-primary font-semibold flex justify-center items-center gap-2"
-                    target="_blank"
-                  >
-                    <span>Ver demo</span>
-                    <FaGlobe size={35} className="text-blue-400" />
-                  </a>
+                  <div className="bg-accent cursor-pointer w-45 p-2 rounded-lg">
+                    <a
+                      href={project.demoURL}
+                      className="text-primary font-semibold flex justify-center items-center gap-2"
+                      target="_blank"
+                    >
+                      <FaGlobe size={30} className="text-white" />
+                      <span>Ver demo</span>
+                    </a>
+                  </div>
                 </div>
               </div>
-            </div>
-          </CardContent>
-        </Card>
-      ))}
+            </CardContent>
+
+            <CardFooter>
+               <p className='text-sm'><span className='font-semibold'>Tecnologias: </span>{project.technologies.join(', ')}</p>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
     </div>
   );
 };
--- src/constants/Projects.ts
+++ src/constants/Projects.ts
@@ -1,9 +1,13 @@
 import coverNewsFinder from '../assets/imgs/cover_news_finder.png';
+import coverTaskManager from '../assets/imgs/cover_task_mananger.png';
+import coverDevNotes from '../assets/imgs/cover_dev_notes.png';
+import coverLandingPage from '../assets/imgs/cover_landing_page.png';
 
 interface Projects {
   id: number;
   name: string;
   description: string;
+  technologies: string[];
   imgURL: string;
   repositorieURL: string;
   demoURL: string;
@@ -12,14 +16,41 @@ interface Projects {
 const PROJECTS: Projects[] = [
   {
     id: 1,
+    name: 'Task Mananger FSC',
+    description: 'Aplicação para gerenciamento de tarefas.',
+    imgURL: coverTaskManager,
+    technologies: ['React', 'Javascript', 'Tailwind', 'Vite' , 'JSON Server', 'React Query' , 'React Hook Form'],
+    repositorieURL: "https://github.com/VinicyosFerreira/To-Do-List-Mananger",
+    demoURL: "https://to-do-list-mananger.vercel.app"
+  },
+  {
+    id: 2,
     name: 'News Finder',
     description:
-      `Aplicação que localiza notícias e imagens realizando requisições e filtrando os resultados, e
-       exibindo os resultados em um layout responsivo.`,
+      `Aplicação que localiza notícias .`,
     imgURL: coverNewsFinder,
+    technologies: ['React', 'Javascript', 'CSS', 'Create React App' , 'Airtable' , 'Rest API'],
     repositorieURL: "https://github.com/VinicyosFerreira/Newtab-Newsfinder",
     demoURL: "https://newtabnewsfinder.netlify.app"
   },
+  {
+    id: 3,
+    name: 'Dev Notes',
+    description: 'Aplicação para gerenciamento de notas.',
+    imgURL: coverDevNotes,
+    technologies: ['HTML' , 'CSS' , 'Javascript'],
+    repositorieURL: "https://github.com/VinicyosFerreira/Dev_Notes",
+    demoURL: "https://vinicyosferreira.github.io/Dev_Notes/"
+  },
+  {
+    id: 4,
+    name: 'Landing Page',
+    description: 'Aplicação para simulação de landing page',
+    imgURL: coverLandingPage,
+    technologies: ['React' , 'Javascript' , 'CSS' , 'Formik' , 'Yup', 'Rest API'],
+    repositorieURL: "https://github.com/VinicyosFerreira/Landing-Page",
+    demoURL: "https://vinicyosferreira.github.io/Landing_Page/"
+  }
 ];
 
 export default PROJECTS;

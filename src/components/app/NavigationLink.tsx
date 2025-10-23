c752f34ebc370804b65d2c5375bbcea5f52874cf
Vinicyos Ferreira <vinifr987@gmail.com>
2025-10-23 15:46:05 -0300

    style: ensure styling with Button with link variant and asChild property

--- src/components/app/NavigationLink.tsx
+++ src/components/app/NavigationLink.tsx
@@ -1,36 +1,41 @@
 import type React from 'react';
+import { Button } from '../ui/button';
 
 interface NavigationLinkProps {
   children: React.ReactNode;
   href: string;
   size?: 'sm' | 'xl';
-  bgHover? : 'bgHoverOne' | 'bgHoverTwo';
+  bgHover?: 'bgHoverOne' | 'bgHoverTwo';
   closeMenu?: () => void;
 }
-const NavigationLink = ({ children, href, size = 'sm' , bgHover = 'bgHoverOne' , closeMenu }: NavigationLinkProps) => {
-
+const NavigationLink = ({
+  children,
+  href,
+  size = 'sm',
+  bgHover = 'bgHoverOne',
+  closeMenu,
+}: NavigationLinkProps) => {
   const buttonVariants = {
-    base: 'font-medium text-primary cursor-pointer hover:transition duration-300 p-2 rounded-md',
-    size : {
+    base: 'font-medium text-primary cursor-pointer hover:transition duration-300 p-2 rounded-md hover:no-underline',
+    size: {
       sm: 'text-sm',
-      xl: 'text-xl'
+      xl: 'text-xl',
     },
-    bgHover : {
+    bgHover: {
       bgHoverOne: 'hover:bg-accent',
-      bgHoverTwo: 'hover:bg-muted-foreground/20'
-    }
-  }
+      bgHoverTwo: 'hover:bg-muted-foreground/20',
+    },
+  };
 
   return (
-    <div className='text-center'>
-      <a
-        href={href}
-        className={`${buttonVariants.base} ${buttonVariants.size[size]} ${buttonVariants.bgHover[bgHover]}`}
-        onClick={closeMenu}
-      >
-        {children}
-      </a>
-    </div>
+    <Button
+      variant="link"
+      asChild
+      className={`${buttonVariants.base} ${buttonVariants.size[size]} ${buttonVariants.bgHover[bgHover]}`}
+      onClick={closeMenu}
+    >
+      <a href={href}>{children}</a>
+    </Button>
   );
 };
 

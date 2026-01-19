ec6e367df0624e41421d1cde0d17ecde986fcabf
Vinicyos Ferreira <vinifr987@gmail.com>
2026-01-19 15:18:28 -0300

    feat: add new project to the project list, replacing another project

--- src/constants/Projects.ts
+++ src/constants/Projects.ts
@@ -1,7 +1,7 @@
-import coverNewsFinder from '../assets/imgs/cover_news_finder.png';
 import coverTaskManager from '../assets/imgs/cover_task_mananger.png';
 import coverAparatusBarber from '../assets/imgs/cover_aparatus_barber.png';
-import coverPortalProjetoVoluntario from '../assets/imgs/cover_projeto_portal-voluntarios.jpg'
+import coverPortalProjetoVoluntario from '../assets/imgs/cover_projeto_portal-voluntarios.jpg';
+import coverLandingPage from '../assets/imgs/cover_landing_page.png';
 
 export interface PROJECTS_TYPE {
   id: number;
@@ -35,7 +35,7 @@ export const PROJECTS: PROJECTS_TYPE[] = [
   },
   {
     id: 2,
-    name: 'Task Mananger FSC',
+    name: 'Task Mananger',
     description: `CRUD completo de tarefas, com listagem organizada e um dashboard de status que mostra em tempo real as 
     tarefas disponíveis, concluídas, em andamento e não iniciadas.`,
     imgURL: coverTaskManager,
@@ -53,28 +53,26 @@ export const PROJECTS: PROJECTS_TYPE[] = [
   },
   {
     id: 3,
-    name: 'News Finder',
-    description: `Aplicação que localiza notícias e imagens realizando requisições e filtrando os resultados, e 
-    exibindo os resultados em um layout responsivo.`,
-    imgURL: coverNewsFinder,
-    technologies: [
-      'React',
-      'Javascript',
-      'CSS',
-      'Create React App',
-      'Airtable',
-      'Rest API',
-    ],
-    repositorieURL: 'https://github.com/VinicyosFerreira/Newtab-Newsfinder',
-    demoURL: 'https://newtabnewsfinder.netlify.app',
+    name: 'Landing Page',
+    description: `Aplicação que realiza uma simulação de Landing Page, que seria baseado em formulário e algoritmos , entregando ao usuário uma listagem de produtos alinhadas com seu perfil.`,
+    imgURL: coverLandingPage,
+    technologies: ['React', 'Javascript', 'CSS', 'Formik e Yup', 'Rest API'],
+    repositorieURL: 'https://github.com/VinicyosFerreira/Landing_Page',
+    demoURL: 'https://vinicyosferreira.github.io/Landing_Page',
   },
   {
     id: 4,
     name: 'Portal de Projetos Voluntários',
     description:
       'Portal focado em networking e capacitação profissional através de projetos voluntários reais.',
-    imgURL:  coverPortalProjetoVoluntario,
-    technologies: ['React', 'API REST', 'React Query', 'Tailwind CSS', 'React Router DOM'],
-    demoURL: 'https://volunteer-hub-swart.vercel.app/'
+    imgURL: coverPortalProjetoVoluntario,
+    technologies: [
+      'React',
+      'API REST',
+      'React Query',
+      'Tailwind CSS',
+      'React Router DOM',
+    ],
+    demoURL: 'https://volunteer-hub-swart.vercel.app/',
   },
 ];
